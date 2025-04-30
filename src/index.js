ff4d158f6e9acd35e5ece6f0d643ba990b53ef39
createlsworklappy <justinecastillo7@gmail.com>
2025-04-30 15:55:17 +0900

    func: test module functions for project-data.js, preliminary implementation of project-template.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "./styles.css";
-import { createProject } from "./project-template";
+import { newProject, getAllProjects } from "./project-data";
+import { renderProjectItem } from "./project-template";
 
-createProject();
+newProject("Read Game of Thrones");
+newProject("Play Clair Obscura: Expedition 33");
+console.log(getAllProjects());
+
+// renderProjectItem()
