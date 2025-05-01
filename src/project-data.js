b5429f9b592894642762756b0f46c444c0f63151
createlsworklappy <justinecastillo7@gmail.com>
2025-05-01 15:20:48 +0900

    func: make loadProjects to populate on page load, selectProjectById to specify the project, removeProject to delete projects

--- src/project-data.js
+++ src/project-data.js
@@ -1,6 +1,28 @@
 let projects = []; // initialize projects array to store project objects
 let currentProjId = 0; // initialize id number of projects
 
+function loadProjects() {
+    const storedProjects = localStorage.getItem('projects');
+
+    // if storedProjects exists
+    if (storedProjects) {
+        try {
+            projects = JSON.parse(storedProjects);
+
+            // maxId = accumulator, p = project. 
+            // Math.max chooses the bigger number between maxId and p.id;
+            // maxId starts with -1, and compares it to the first available objects id,
+            // which is always greater than -1. After running the reduce method on every item,
+            // returns the final value of maxId and sets currentProjId to that number
+            currentProjId = projects.reduce((maxId, p) => Math.max(maxId, p.id), -1) + 1;
+        } catch (e) {
+            console.error("Could not parse projects from localStorage", e);
+            projects = [];
+            currentProjId = 0;
+        }
+    }
+}
+
 function newProject(title = "New Project") { // set default value for title if missing
     const newProject = {
         id: currentProjId++, // post increment operator; set id to current value, then increment the variable in one move
@@ -24,9 +46,15 @@ function getAllProjects() {
 }
 
 // implement a function to SELECT a proj for project-template to use
-// function selectProjectById() {
+function selectProjectById(projectId) {
+    const project = projects.find(p => p.id === projectId);
+    return project ? {...project} : null; // once again, return a new object that is a shallow copy of the project object
+    // return null if not found
+}
 
-// }
+function removeProject(projectId) {
+    projects = projects.filter(p => p.id !== projectId); // filter out the project from array
+    saveProjects();
+}
 
-
-export {newProject, getAllProjects};
+export {loadProjects, newProject, getAllProjects, selectProjectById, removeProject};
