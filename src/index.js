3b0347d3c47e337eac8b91d85742d4b7be19a4b6
createlsworklappy <justinecastillo7@gmail.com>
2025-05-01 15:21:05 +0900

    test: test new funcs on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./styles.css";
-import { newProject, getAllProjects } from "./project-data";
-import { renderProjectItem } from "./project-template";
+import { newProject, getAllProjects, selectProjectById, removeProject } from "./project-data";
+import { loadApp, renderProjectItem, createModal } from "./project-template";
 
 newProject("Read Game of Thrones");
 newProject("Play Clair Obscura: Expedition 33");
 console.log(getAllProjects());
 
-// renderProjectItem()
+console.log(selectProjectById(0));
+removeProject(0);
+console.log(selectProjectById(1));
+console.log(selectProjectById(0));
+loadApp();
+// renderProjectItem();
