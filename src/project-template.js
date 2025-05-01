5628e4ee86b88375f7ac3830539809dcdc1ed596
createlsworklappy <justinecastillo7@gmail.com>
2025-05-01 15:19:51 +0900

    func: implement loadApp for page load behavior, createAddButton for adding to-dos, createModal for modal elements,

--- src/project-template.js
+++ src/project-template.js
@@ -1,3 +1,40 @@
+function loadApp() {
+    const projFolder = document.querySelector("#projFolder")
+
+    if (!projFolder.hasChildNodes()) {
+        const centerButton = createAddButton();
+        centerButton.id = "centerButton";
+        const caption = document.createElement("p");
+        caption.textContent = "Add your first to-do-note!"
+        centerButton.insertBefore(caption, centerButton.firstChild);
+        projFolder.append(centerButton);
+    } else {
+        console.log("wenk");
+    }
+}
+
+function createAddButton() {
+    const addButton = document.createElement("button");
+    addButton.classList.add("addButton");
+    addButton.textContent = "+";
+    addButton.addEventListener("click", createModal);
+    return addButton;
+}
+
+function createModal() {
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    const modalContainer = document.createElement("div");
+    modalContainer.classList.add("modalContainer");
+    const body = document.querySelector("body");
+    modal.append(modalContainer);
+    body.append(modal);
+}
+// function to view the note modal
+// function showModal() {
+//     const modal = document.
+// }
+
 function renderProjectItem(currentProjectItem) {
     const projFolder = document.querySelector("#projFolder")
     const project = currentProjectItem;
@@ -9,4 +46,4 @@ function renderProjectItem(currentProjectItem) {
     projFolder.append(projectCard);
 }
 
-export {renderProjectItem}
+export {loadApp, renderProjectItem, createModal}
