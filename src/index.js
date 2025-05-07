bb041024a4672ebf345b9b1afa2990313e006eb3
createlsworklappy <justinecastillo7@gmail.com>
2025-05-07 13:28:35 +0900

    Misc: update imported functions from project-template
    set base modal elements on HTML

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { newProject, getAllProjects, selectProjectById, removeProject } from "./project-data";
-import { loadApp, renderProjectItem, createModal } from "./project-template";
+import { loadApp, renderProjectItem, showModal } from "./project-template";
 
 newProject("Read Game of Thrones");
 newProject("Play Clair Obscura: Expedition 33");
