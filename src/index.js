7f056a5d42c305443d8ada89d33324ca7d56628b
createlsworklappy <justinecastillo7@gmail.com>
2025-05-14 14:45:45 +0900

    Misc: adjustments on syntax/naming of functions and classes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./styles.css";
 import { newProject, getAllProjects, selectProjectById, removeProject } from "./project-data";
-import { loadApp, renderProjectItem, showModal } from "./project-template";
+import { loadApp, renderProjectCard } from "./project-template";
 
-newProject("Read Game of Thrones");
-newProject("Play Clair Obscura: Expedition 33");
 console.log(getAllProjects());
 
 console.log(selectProjectById(0));
@@ -11,4 +9,3 @@ removeProject(0);
 console.log(selectProjectById(1));
 console.log(selectProjectById(0));
 loadApp();
-// renderProjectItem();
