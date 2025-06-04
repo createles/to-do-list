fc3c79cc1002c6b9d18f8a84992987b73a343d0a
createlsworklappy <justinecastillo7@gmail.com>
2025-06-04 15:04:50 +0900

    Misc: clean up index file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import "./styles.css";
 import { newProject, selectProjectById, removeProject } from "./project-data";
 import { loadApp, renderProjectCard } from "./project-template";
 
-console.log(selectProjectById(0));
-removeProject(0);
-console.log(selectProjectById(1));
-console.log(selectProjectById(0));
 loadApp();
